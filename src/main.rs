6bfed0e6d4dc91fbba1ceea4303d5c9a56a73d46
Matthew Miner <mminer237@gmail.com>
2023-10-01 14:26:59 -0500

    Initial text reading

--- src/main.rs
+++ src/main.rs
@@ -18,25 +18,36 @@ fn main() {
     siv.borrow_mut().add_layer(
         LinearLayout::horizontal()
             .child(build_selector(Rc::clone(&bible)))
-            .child(
-                ScrollView::new(TextView::new("In the beginning, ..."))
-            )
+            .child(build_text_view(Rc::clone(&bible), "Genesis".to_owned(), 1))
     );
 
     (move |bible, siv: Rc<RefCell<CursiveRunnable>>| {
+        /* Add listener to book selection */
         let mut book_view = siv.borrow_mut().find_name::<SelectView<String>>("book_view").unwrap();
         (|bible| {
             (*book_view).set_on_select(move |s, book_name| {
                 rebuild_chapter_selector(s, Rc::clone(&bible), book_name.to_string());
                 let chapter_number = s.find_name::<SelectView<String>>("chapter_view").unwrap().selected_id().unwrap();
-                rebuild_verse_selector(s, Rc::clone(&bible), book_name.to_string(), chapter_number);
+                rebuild_verse_selector(s, Rc::clone(&bible), book_name, &chapter_number);
+                rebuild_text_view(s, Rc::clone(&bible), book_name, &chapter_number);
             });
         })(Rc::clone(&bible));
         let mut chapter_view = siv.borrow_mut().find_name::<SelectView<String>>("chapter_view").unwrap();
-        (*chapter_view).set_on_select(move |s, chapter| {
-            let book_name = s.find_name::<SelectView<String>>("book_view").unwrap().selection().unwrap().to_string();
-            rebuild_verse_selector(s, Rc::clone(&bible), book_name, chapter.parse::<usize>().unwrap() - 1);
-        });
+
+        /* Add listener to chapter selection */
+        (|bible| {
+            (*chapter_view).set_on_select(move |s, chapter| {
+                let book_name = s.find_name::<SelectView<String>>("book_view").unwrap().selection().unwrap().to_string();
+                let chapter_number = chapter.parse::<usize>().unwrap() - 1;
+                rebuild_verse_selector(s, Rc::clone(&bible), &book_name, &chapter_number);
+                rebuild_text_view(s, Rc::clone(&bible), &book_name, &chapter_number);
+            });
+        })(Rc::clone(&bible));
+        
+        /* Add listener to verse selection */
+        (|bible| {
+
+        })(Rc::clone(&bible));
     })(Rc::clone(&bible), Rc::clone(&siv));
 
     siv.borrow_mut().run();
@@ -85,10 +96,29 @@ fn build_verse_selector<'a>(bible: Rc<Bible>, book_view: &SelectView, chapter_vi
     return verse_view;
 }
 
-fn rebuild_verse_selector<'a>(siv: &mut Cursive, bible: Rc<Bible>, book_name: String, chapter_number: usize) {
+fn rebuild_verse_selector<'a>(siv: &mut Cursive, bible: Rc<Bible>, book_name: &String, chapter_number: &usize) {
     let mut verse_view = siv.find_name::<SelectView<String>>("verse_view").unwrap();
     verse_view.clear();
-    let book_number = (&bible.books).into_iter().position(|x| x.name == book_name).unwrap();
-    verse_view.add_all_str(bible.books[book_number].chapters[chapter_number].verses.iter().map(|x| x.number.to_string()));
+    let book_number = (&bible.books).into_iter().position(|x| x.name == *book_name).unwrap();
+    verse_view.add_all_str(bible.books[book_number].chapters[*chapter_number].verses.iter().map(|x| x.number.to_string()));
     verse_view.set_selection(0);
+}
+
+fn build_text_view(bible: Rc<Bible>, book_name: String, chapter_number: usize) -> ScrollView<NamedView<LinearLayout>> {
+    let mut linear_layout = LinearLayout::vertical();
+    set_verses(&mut linear_layout, bible, &book_name, chapter_number);
+    return ScrollView::new(NamedView::new("text_view", linear_layout));
+}
+
+fn rebuild_text_view(siv: &mut Cursive, bible: Rc<Bible>, book_name: &String, chapter_number: &usize) {
+    let mut text_view = siv.find_name::<LinearLayout>("text_view").unwrap();
+    set_verses(&mut text_view, bible, book_name, *chapter_number);
+}
+
+fn set_verses(view: &mut LinearLayout, bible: Rc<Bible>, book_name: &String, chapter_number: usize) {
+    view.clear();
+    let book_number = (&bible.books).into_iter().position(|x| x.name == *book_name).unwrap();
+    bible.books[book_number].chapters[chapter_number].verses.iter().for_each(|x| {
+        view.add_child(TextView::new(format!("{} {}", x.number, x.text)));
+    });
 }
